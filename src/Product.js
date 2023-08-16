cc37022625e2cc78a092c26bf7f2dfa1822a2b5e
Deepak Gurjar <deepak.gurjar@heliostechservices.com>
2023-08-16 15:35:29 +0530

    add remove cart functionality

--- src/Product.js
+++ src/Product.js
@@ -1,36 +1,65 @@
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View, Image} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
 
-import { addToCart } from './redux/action';
-import { useDispatch } from 'react-redux';
+import { addToCart ,removeFromCart} from './redux/action';
+import { useDispatch, useSelector } from 'react-redux';
+import { reducer } from './redux/reducer';
 const Product = props => {
-  const items = props.item;
-  const dispatch= useDispatch();
-  // if (!items) {
+  const item = props.item;
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.reducer);
+
+  const [isAdded, setIsAdded] = useState(false);
+  // if (!item) {
   //   return null;
   // }    
-  const handleAddToCart =(items)=>{
-      // console.warn("called  ",items)
-      dispatch(addToCart(items))
-  }       
+  const handleAddToCart = (item) => {
+    // console.warn("called  ",item)
+    dispatch(addToCart(item))
+  }
+  const handleRemoveFromCart= (item)=>{
+    dispatch(removeFromCart(item.name))
+  }
+  useEffect(() => {
+    // if (cartItems && cartItems.length) {
+    //   cartItems.forEach((elements) => {
+    //     if (elements.name === item.name) {
+    //       setIsAdded(true)
+    //     }
+    //   })
+    // }
+
+     let result = cartItems.filter((element) => {
+      return element.name === item.name
+    })
+    if (result.length) {
+      setIsAdded(true)
+    }
+    else{
+      setIsAdded(false) 
+    }
+  },[cartItems])
   return (
     <View
       style={{
         alignItems: 'center',
-        borderBottomColor:"black",
+        borderBottomColor: "black",
         borderBottomWidth: 4,
         padding: 10,
       }}>
-      <Text style={{fontSize: 24}}>{items.name}</Text>
-      <Text style={{fontSize: 24}}>{items.price}</Text>
-      <Text style={{fontSize: 24}}>{items.color}</Text>
-      {/* <Image style={{width: 100, height: 100}} source={{uri: items.image}} /> */}
-      <Image 
-      source={items.image }
-      style={{width: 100, height: 100}}>
+      <Text style={{ fontSize: 24 }}>{item.name}</Text>
+      <Text style={{ fontSize: 24 }}>{item.price}</Text>
+      <Text style={{ fontSize: 24 }}>{item.color}</Text>
+      {/* <Image style={{width: 100, height: 100}} source={{uri: item.image}} /> */}
+      <Image
+        source={item.image}
+        style={{ width: 100, height: 100 }}>
 
-      </Image> 
-      <Button title="ADD TO CART" onPress={()=> handleAddToCart(items)}  ></Button>
+      </Image>
+      {
+        isAdded ? <Button title="REMOVE TO CART" onPress={() => handleRemoveFromCart(item)}  ></Button> : <Button title="ADD TO CART" onPress={() => handleAddToCart(item)}  ></Button>
+      }
+
     </View>
   );
 };
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,4 @@
-import ADD_TO_CART from './constant';
+import {ADD_TO_CART, REMOVE_FROM_CART} from './constant';
 
 const initialState = [];
 
@@ -6,6 +6,12 @@ export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
       return [...state, action.data];
+
+    case REMOVE_FROM_CART:
+      let resultedItem= state.filter((item)=>{
+        return item.name!=action.data
+      })
+      return [...resultedItem];
     default:
         return state
   }
